6cf7d989ceba83d1ab16fe446102ae97b8ff91b5
perfectacle <perfectacle@naver.com>
2017-04-04 08:25:38 +0900

    fix babel preset latest to babel preset env

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -4,7 +4,7 @@ export default class About extends Component {
   render() {
     return(
       <div>
-        <h2>Aboutbzsqaaa</h2>
+        <h2>About</h2>
         <div>{this.props.children}</div>
       </div>
     );
